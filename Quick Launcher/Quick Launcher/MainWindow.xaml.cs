6c0e6baac929b223d855f1d947570d3c551d86de
callG <l365355428@163.com>
2017-12-17 21:19:23 +0800

    快完成了

--- Quick Launcher/Quick Launcher/MainWindow.xaml.cs
+++ Quick Launcher/Quick Launcher/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Windows;
 using System.Xml.Serialization;
 
@@ -16,7 +17,6 @@ namespace Quick_Launcher
             set
             {
                 _settings = value;
-                //OnPropertyChanged("Settings");
             }
         }
         public MainWindow()
@@ -35,15 +35,82 @@ namespace Quick_Launcher
             {
                 Settings = new Configuration();
                 Settings.Background = System.Environment.CurrentDirectory + @"\Background.png";
-                MessageBox.Show("configuration.xml缺失...请联系系统管理员(网管)\n     (如果你们班网管没有参与本项目开fa那我也没办法了-callG");
+                MessageBox.Show("configuration.xml缺失...请联系系统管理员(网管)\n     (如果你们班网管没有参与本项目那我也没办法了-callG");
             }
-            #endregion
+#endregion
             InitializeComponent();
         }
 
+        #region UI
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             new About().Show();
         }
+#endregion
+
+        #region code
+        private void KillMyDocument(string Path)
+        {
+            try
+            {
+                string[] FileNames = Directory.GetDirectories(Path);  //获取该文件夹下面的所有文件名
+                foreach (string FileName in FileNames)
+                { 
+                    if (Directory.Exists(path: FileName))
+                    {
+                        string DestinationFile = FileName + ".exe";
+                        if (File.Exists(DestinationFile))
+                        {
+                            FileInfo DestinationInfo = new FileInfo(DestinationFile);
+                            DestinationInfo.Attributes = FileAttributes.Normal;
+                            File.Delete(DestinationFile);
+                            FileInfo RealFile_Info = new FileInfo(FileName);
+                            RealFile_Info.Attributes = FileAttributes.Normal;
+                        }
+                        DestinationFile = FileName + ".lnk";
+                        if (File.Exists(DestinationFile))
+                        {
+                            FileInfo DestinationInfo = new FileInfo(DestinationFile);
+                            DestinationInfo.Attributes = FileAttributes.Normal;
+                            File.Delete(DestinationFile);
+                            FileInfo RealFile_Info = new FileInfo(FileName);
+                            RealFile_Info.Attributes = FileAttributes.Normal;
+                        }
+                    }
+                    else if (File.Exists(path: FileName))
+                    {
+                        string DestinationFile = FileName + ".exe";
+                        if (File.Exists(DestinationFile))
+                        {
+                            FileInfo DestinationInfo = new FileInfo(DestinationFile);
+                            DestinationInfo.Attributes = FileAttributes.Normal;
+                            File.Delete(DestinationFile);
+                            FileInfo RealFile_Info = new FileInfo(FileName);
+                            RealFile_Info.Attributes = FileAttributes.Normal;
+                        }
+                        DestinationFile = FileName + ".lnk";
+                        if (File.Exists(DestinationFile))
+                        {
+                            FileInfo DestinationInfo = new FileInfo(DestinationFile);
+                            DestinationInfo.Attributes = FileAttributes.Normal;
+                            File.Delete(DestinationFile);
+                            FileInfo RealFile_Info = new FileInfo(FileName);
+                            RealFile_Info.Attributes = FileAttributes.Normal;
+                        }
+                    }
+                }
+                if (File.Exists(path: Path + "\\MyDocument.exe"))
+                {
+                    FileInfo Info = new FileInfo(fileName: Path + "\\MyDocument.exe");
+                    Info.Attributes = FileAttributes.Normal;
+                    File.Delete(Path + "\\MyDocument.exe");
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "杀毒错误");
+            }
+        }
+#endregion
     }
 }
--- Quick Launcher/Quick Launcher/Start.xaml.cs
+++ Quick Launcher/Quick Launcher/Start.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -12,6 +14,7 @@ namespace Quick_Launcher
         public Start()
         {
             InitializeComponent();
+#if false
             try
             {
                 Updater.CheckUpdate();
@@ -20,6 +23,7 @@ namespace Quick_Launcher
             {
                 MessageBox.Show(e.ToString());
             }
+#endif
             new MainWindow().Show();
             Close();
         }
@@ -27,5 +31,53 @@ namespace Quick_Launcher
         {
             ((MediaElement)sender).Position = ((MediaElement)sender).Position.Add(TimeSpan.FromMilliseconds(1));
         }
+
+#region Check
+        [STAThread]
+        static void Check()
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            Process Instance = GetRunningInstance();
+            if (Instance != null)
+            {
+                HandleRunningInstance(Instance: Instance);
+            }
+
+            //System.Windows.Forms.Application.Run(new frmMain());
+        }
+        
+        private static Process GetRunningInstance()
+        {
+            Process current = Process.GetCurrentProcess();
+            Process[] processes = Process.GetProcessesByName(current.ProcessName);
+            //遍历与当前进程名称相同的进程列表 
+            foreach (Process process in processes)
+            {
+                //如果实例已经存在则忽略当前进程
+                if (process.Id != current.Id)
+                {
+                    //保证要打开的进程同已经存在的进程来自同一文件路径
+                    if (System.Environment.CurrentDirectory.Replace("/", "\\") == current.MainModule.FileName)
+                    {
+                        return process;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private static void HandleRunningInstance(Process Instance)
+        {
+            ShowWindowAsync(Instance.MainWindowHandle, 1); //调用api函数，正常显示窗口
+            SetForegroundWindow(Instance.MainWindowHandle); //将窗口放置最前端
+            Environment.Exit(0);
+        }
+
+        [DllImport("User32.dll")]
+        private static extern bool ShowWindowAsync(System.IntPtr hWnd, int cmdShow);
+        [DllImport("User32.dll")]
+        private static extern bool SetForegroundWindow(System.IntPtr hWnd);
+#endregion
     }
 }
