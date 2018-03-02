9b86aa74a3a3da06a4a75c2a4d7e2dcfa6f32958
callG <l365355428@163.com>
2018-03-02 11:34:59 +0800

    改布局

---
+++ Quick Launcher/Quick Launcher/UDisk.cs
@@ -0,0 +1,16 @@
+﻿namespace Quick_Launcher
+{
+    internal class UDisk
+    {
+        internal string Letter;
+        internal string Name;
+        internal string Size;
+
+        internal UDisk(string letter, string name, string size)
+        {
+            Letter = letter;
+            Name = name;
+            Size = size;
+        }
+    }
+}
